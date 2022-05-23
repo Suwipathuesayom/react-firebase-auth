3473d79c1fb4350d6e9eb04dd93e3af9daedbb27
Suwipathuesayom <suwipa.thu@gmail.com>
2022-05-23 21:31:03 +0700

    commit auth-react

---
+++ react-firebase-auth-main/src/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyAKL6I7Uw6lj5cPfrGqpOMSpNrSNBJcoEk",
+  authDomain: "react-auth-3a88e.firebaseapp.com",
+  projectId: "react-auth-3a88e",
+  storageBucket: "react-auth-3a88e.appspot.com",
+  messagingSenderId: "284354848707",
+  appId: "1:284354848707:web:2e43c8a2b957d833233f76",
+  measurementId: "G-MZ8BHTKDML",
+});
+
+export default firebaseConfig;
